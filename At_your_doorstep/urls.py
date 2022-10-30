8c6daa3b1cdf5844746bb59633de2fee7701ffc0
Prabhsimran Singh <prabhsimransingh24092@gmail.com>
2022-10-30 22:36:59 +0530

    this is my first commit

--- At_your_doorstep/urls.py
+++ At_your_doorstep/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from At_your_doorstep.settings import MEDIA_ROOT
+
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
